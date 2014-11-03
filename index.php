d0e7a8e6dc8bc1940e0874c07c0ca4c9be4a1ff3
Van Doan <vdoan09@gmail.com>
2014-11-02 19:25:15 -0500

    launching project sparrow

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html> 
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP Tutorial</title>
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="style/style.css">
+	</head>
+
+	<body> 
+		<?php include_once("template_pageTop.php"); ?>
+		<div id="pageMiddle">
+			&nbsp; 		Hello world
+
+		</div>
+		<?php include_once("template_pageBottom.php"); ?>
+	</body>
+</html>
---
+++ template_pageTop.php
@@ -0,0 +1,25 @@
+<div id="pageTop">
+  <div id="pageTopWrap">
+    <div id="pageTopLogo">
+      <a href="#">
+        <img src="images/logo.jpg" alt="logo" style="width:30px;" title="Web Intersect 2.0">
+      </a>
+    </div>
+    <div id="pageTopRest">
+      <div id="menu1">
+        <div>
+          <a href="#">Sign Up / Log In</a>
+        </div>
+      </div>
+      <div id="menu2">
+        <div>
+          <a href="#">
+            <img src="images/home.png" style="width:30px" alt="home" title="Home">
+          </a>
+          <a href="#">Menu_Item_1</a>
+          <a href="#">Menu_Item_2</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
