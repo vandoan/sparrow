d7e3a2846a2c6955600835e9c1517b5745bd8ddd
Van Doan <vdoan09@gmail.com>
2014-11-03 05:53:07 -0500

    javascript modules

---
+++ signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html> 
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP Tutorial</title>
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="style/style.css">
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<script src="js/main.js"></script>
+	<script src="js/ajax.js"></script>
+	<script src="js/fade.js"></script>
+	</head>
+
+	<body> 
+		<?php include_once("template_pageTop.php"); ?>
+		<div id="pageMiddle">
+			&nbsp; 		Hello world
+
+		</div>
+		<?php include_once("template_pageBottom.php"); ?>
+	</body>
+</html>
